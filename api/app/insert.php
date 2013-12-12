0a25daec399f9ce21e2b8905bf2b835ff8084f6b
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-12 03:50:09 +0100

    Bugfix

--- api/app/insert.php
+++ api/app/insert.php
@@ -116,8 +116,7 @@ $a->setExecute(function() use ($a)
 	$app = $runtime . '-' . $app;
 	
 	$extra = array();
-	$extra['instances'] = array(array('memory' => '128', 'cpu' => 1));
-	$extra['branches'] = array('master');
+	$extra['branches'] = array('master' => array('instances'=>array(array('memory' => '128', 'cpu' => 1))));
 	
 	$dn = ldap::buildDN(ldap::APP, $domain, $app);
 	$params = array('dn' => $dn, 'uid' => $app, 'userPassword' => $pass, 'domain' => $domain, 'description' => json_encode($extra), 'owner' => $user_dn);
