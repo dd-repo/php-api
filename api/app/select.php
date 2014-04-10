be2d0a94e80a6e33b9fef6fd152320bc1423a864
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-10 19:21:12 +0200

    Bugfix

--- api/app/select.php
+++ api/app/select.php
@@ -187,7 +187,7 @@ $a->setExecute(function() use ($a)
 		{
 			foreach( $extra['branches'] as $key => $value )
 			{
-				$sql = "SELECT b.branch_name, b.app_id, b.app_name, a.app_tag FROM service_branch b LEFT JOIN apps a ON(a.app_id = b.app_id) WHERE app_id = '{$result['uidNumber']}' AND branch_name = '{$key}'";
+				$sql = "SELECT b.branch_name, b.app_id, b.app_name, a.app_tag FROM service_branch b LEFT JOIN apps a ON(a.app_id = b.app_id) WHERE b.app_id = '{$result['uidNumber']}' AND b.branch_name = '{$key}'";
 				$services = $GLOBALS['db']->query($sql);
 				
 				$infos['branches'][$key]['services'] = $services;
@@ -325,7 +325,7 @@ $a->setExecute(function() use ($a)
 			{
 				foreach( $extra['branches'] as $key => $value )
 				{
-					$sql = "SELECT b.branch_name, b.app_id, b.app_name, a.app_tag FROM service_branch b LEFT JOIN apps a ON(a.app_id = b.app_id) WHERE app_id = '{$r['uidNumber']}' AND branch_name = '{$key}'";
+					$sql = "SELECT b.branch_name, b.app_id, b.app_name, a.app_tag FROM service_branch b LEFT JOIN apps a ON(a.app_id = b.app_id) WHERE b.app_id = '{$r['uidNumber']}' AND b.branch_name = '{$key}'";
 					$services = $GLOBALS['db']->query($sql);
 				
 					$infos['branches'][$key]['services'] = $services;
