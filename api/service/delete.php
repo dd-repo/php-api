e6ddaaa3367945ec472d01c66b3bc622b7930666
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-11 16:35:19 +0200

    Bugfix

--- api/service/delete.php
+++ api/service/delete.php
@@ -81,8 +81,8 @@ $a->setExecute(function() use ($a)
 	{
 		case 'mysql':
 			$link = new mysqli($GLOBALS['CONFIG']['MYSQL_ROOT_HOST'], $GLOBALS['CONFIG']['MYSQL_ROOT_USER'], $GLOBALS['CONFIG']['MYSQL_ROOT_PASSWORD'], 'mysql', $GLOBALS['CONFIG']['MYSQL_ROOT_PORT']);
-			link->query("DROP USER '{$service}'");
-			link->query("DROP DATABASE `{$service}`");
+			$link->query("DROP USER '{$service}'");
+			$link->query("DROP DATABASE `{$service}`");
 		break;
 		case 'pgsql':
 			$command = "/dns/tm/sys/usr/local/bin/drop-db-pgsql {$service}";
